47fe9e2a2fd7370a58c009cc930fa0d15de36d97
Dranemo <ycaillot@gaming.tech>
2024-11-23 18:58:42 +0100

    fixed post processing effects and added vignette when hiding

--- Assets/Scripts/InteractableItems/HidingPlace.cs
+++ Assets/Scripts/InteractableItems/HidingPlace.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class HidingPlace : Interactable
 {
     [SerializeField] List<GameObject> virtualCams;
+    [SerializeField] VolumeProfile volumeProfile;
+    private Vignette vignette;
+
     GameObject closestVirtualCam;
+
+    Coroutine intensityCoroutine;
+    bool coroutineRunningUp = false;
+    bool coroutineRunningDown = false;
+
 
     GameObject player;
     bool isHidden = false;
@@ -20,6 +31,31 @@ public class HidingPlace : Interactable
         }
         player = PlayerSingleton.GetPlayer();
 
+        // Récupérer le composant Vignette du VolumeProfile
+        if (volumeProfile.TryGet(out Vignette v))
+        {
+            vignette = v;
+        }
+        else
+        {
+            Debug.LogError("Vignette n'est pas trouvé dans le VolumeProfile.");
+        }
+
+        intensityCoroutine = null;
+
+    }
+
+    private void Update()
+    {
+        if (isHidden && !coroutineRunningUp)
+        {
+            coroutineRunningUp = true;
+            coroutineRunningDown = false;
+
+            if (intensityCoroutine != null)
+                StopCoroutine(intensityCoroutine);
+            intensityCoroutine = StartCoroutine(ChangeVignetteIntensity(vignette.intensity.value, 1f, 15f));
+        }
     }
 
 
@@ -29,6 +65,7 @@ public class HidingPlace : Interactable
         {
             isHidden = true;
             player.SetActive(false);
+            Inventory.Instance().GetItemInHand().gameObject.SetActive(false);
 
             float closesDist = Mathf.Infinity;
             foreach (GameObject cam in virtualCams)
@@ -43,13 +80,39 @@ public class HidingPlace : Interactable
             closestVirtualCam.SetActive(!closestVirtualCam.activeSelf);
             Debug.Log("Interacting with hiding place");
         }
-        else if(isHidden)
+        else if(isHidden && !coroutineRunningDown)
         {
+            coroutineRunningDown = true;
+            coroutineRunningUp = false;
+
+            if (intensityCoroutine != null)
+                StopCoroutine(intensityCoroutine);
+            intensityCoroutine = StartCoroutine(ChangeVignetteIntensity(vignette.intensity.value, 0.3f, 1f));
+
             isHidden = false;
             PlayerSingleton.GetPlayer().SetActive(true);
+            Inventory.Instance().GetItemInHand().gameObject.SetActive(true);
 
             closestVirtualCam.SetActive(!closestVirtualCam.activeSelf);
             Debug.Log("Interacting with hiding place");
         }
+    }
+
+
+    IEnumerator ChangeVignetteIntensity(float start, float end, float duration)
+    {
+        float t = 0;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            vignette.intensity.value = Mathf.Lerp(start, end, t / duration);
+
+            yield return null;
+        }
+
+        intensityCoroutine = null;
+        coroutineRunningUp = false;
+        coroutineRunningDown = false;
     }
 }
